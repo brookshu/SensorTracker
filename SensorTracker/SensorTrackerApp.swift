f573301d09166a9e7b648e9eca193f85c2114005
Vasco Xu <vamiguelxu@gmail.com>
2024-09-30 19:47:00 -0500

    Initial Commit

---
+++ SensorTracker/SensorTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SensorTrackerApp.swift
+//  SensorTracker
+//
+//  Created by Vasco Xu on 9/30/24.
+//
+
+import SwiftUI
+
+@main
+struct SensorTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
